7ad784baeb748bfa5da10d7f3195a85965f7b496
Medard <rmedard5@gmail.com>
2018-04-17 18:03:24 +0200

    EC

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -104,32 +104,34 @@
     <div class="container-fluid">
         <div class="container">
             <div class="<?php print $navbar_classes; ?>">
-                <div class="navbar-header">
-                    <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
-                        <button type="button" class="navbar-toggle"
-                                data-toggle="collapse"
-                                data-target="#navbar-collapse">
-                            <span class="sr-only"><?php print t('Toggle navigation'); ?></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                        </button>
-                    <?php endif; ?>
-                    <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
-                        <div class="navbar-collapse collapse" id="navbar-collapse">
-                            <nav role="navigation">
-                                <?php if (!empty($primary_nav)): ?>
-                                    <?php print render($primary_nav); ?>
-                                <?php endif; ?>
-                                <?php if (!empty($secondary_nav)): ?>
-                                    <?php print render($secondary_nav); ?>
-                                <?php endif; ?>
-                                <?php if (!empty($page['navigation'])): ?>
-                                    <?php print render($page['navigation']); ?>
-                                <?php endif; ?>
-                            </nav>
-                        </div>
-                    <?php endif; ?>
+                <div class="container">
+                    <div class="navbar-header">
+                        <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
+                            <button type="button" class="navbar-toggle"
+                                    data-toggle="collapse"
+                                    data-target="#navbar-collapse">
+                                <span class="sr-only"><?php print t('Toggle navigation'); ?></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+                        <?php endif; ?>
+                        <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
+                            <div class="navbar-collapse collapse" id="navbar-collapse">
+                                <nav role="navigation">
+                                    <?php if (!empty($primary_nav)): ?>
+                                        <?php print render($primary_nav); ?>
+                                    <?php endif; ?>
+                                    <?php if (!empty($secondary_nav)): ?>
+                                        <?php print render($secondary_nav); ?>
+                                    <?php endif; ?>
+                                    <?php if (!empty($page['navigation'])): ?>
+                                        <?php print render($page['navigation']); ?>
+                                    <?php endif; ?>
+                                </nav>
+                            </div>
+                        <?php endif; ?>
+                    </div>
                 </div>
             </div>
 
