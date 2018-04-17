0fe429ea5b6193eef474b38030091fe395c031e3
Medard <rmedard5@gmail.com>
2018-04-17 16:35:33 +0200

    EC

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -75,6 +75,14 @@
  */
 ?>
 <header id="navbar" role="banner" class="<?php print $navbar_classes; ?>">
+    <div class="row">
+        <div class="col-md-3">
+            <?php print render($page['jirheader_logo']); ?>
+        </div>
+        <div class="col-md-9">
+            <?php print render($page['jirheader_banner']); ?>
+        </div>
+    </div>
     <div class="<?php print $container_class; ?>">
         <div class="navbar-header">
             <?php if ($logo): ?>
@@ -91,44 +99,39 @@
                    title="<?php print t('Home'); ?>"><?php print $site_name; ?></a>
             <?php endif; ?>
 
-            <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
-                <button type="button" class="navbar-toggle"
-                        data-toggle="collapse" data-target="#navbar-collapse">
-                    <span class="sr-only"><?php print t('Toggle navigation'); ?></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-            <?php endif; ?>
+<!--            --><?php //if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
+<!--                <button type="button" class="navbar-toggle"-->
+<!--                        data-toggle="collapse" data-target="#navbar-collapse">-->
+<!--                    <span class="sr-only">--><?php //print t('Toggle navigation'); ?><!--</span>-->
+<!--                    <span class="icon-bar"></span>-->
+<!--                    <span class="icon-bar"></span>-->
+<!--                    <span class="icon-bar"></span>-->
+<!--                </button>-->
+<!--            --><?php //endif; ?>
+            <?php print render($page['jirheader_banner']); ?>
         </div>
 
-        <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
-            <div class="navbar-collapse collapse" id="navbar-collapse">
-                <nav role="navigation">
-                    <?php if (!empty($primary_nav)): ?>
-                        <?php print render($primary_nav); ?>
-                    <?php endif; ?>
-                    <?php if (!empty($secondary_nav)): ?>
-                        <?php print render($secondary_nav); ?>
-                    <?php endif; ?>
-                    <?php if (!empty($page['navigation'])): ?>
-                        <?php print render($page['navigation']); ?>
-                    <?php endif; ?>
-                </nav>
-            </div>
-        <?php endif; ?>
+        <?php print render($page['jirheader_banner']); ?>
+
+<!--        --><?php //if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
+<!--            <div class="navbar-collapse collapse" id="navbar-collapse">-->
+<!--                <nav role="navigation">-->
+<!--                    --><?php //if (!empty($primary_nav)): ?>
+<!--                        --><?php //print render($primary_nav); ?>
+<!--                    --><?php //endif; ?>
+<!--                    --><?php //if (!empty($secondary_nav)): ?>
+<!--                        --><?php //print render($secondary_nav); ?>
+<!--                    --><?php //endif; ?>
+<!--                    --><?php //if (!empty($page['navigation'])): ?>
+<!--                        --><?php //print render($page['navigation']); ?>
+<!--                    --><?php //endif; ?>
+<!--                </nav>-->
+<!--            </div>-->
+<!--        --><?php //endif; ?>
     </div>
 </header>
 
 <div class="main-container <?php print $container_class; ?>">
-    <div class="row">
-        <div class="col-md-3">
-            <?php print render($page['jirheader_logo']); ?>
-        </div>
-        <div class="col-md-9">
-            <?php print render($page['jirheader_banner']); ?>
-        </div>
-    </div>
 
     <header role="banner" id="page-header">
         <?php if (!empty($site_slogan)): ?>
