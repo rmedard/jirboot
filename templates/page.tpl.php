ae3c02d891bc353f7ca105a1dae0e30e8c734d9b
Medard <rmedard5@gmail.com>
2018-04-17 17:52:39 +0200

    EC

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -133,13 +133,13 @@
                 </div>
             </div>
 
-            <header role="banner" id="page-header">
-                <?php if (!empty($site_slogan)): ?>
-                    <p class="lead"><?php print $site_slogan; ?></p>
-                <?php endif; ?>
-
-                <?php print render($page['header']); ?>
-            </header> <!-- /#page-header -->
+<!--            <header role="banner" id="page-header">-->
+<!--                --><?php //if (!empty($site_slogan)): ?>
+<!--                    <p class="lead">--><?php //print $site_slogan; ?><!--</p>-->
+<!--                --><?php //endif; ?>
+<!---->
+<!--                --><?php //print render($page['header']); ?>
+<!--            </header> <!-- /#page-header -->-->
         </div>
     </div>
 
