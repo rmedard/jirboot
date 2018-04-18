0d2d9cf376ab55c50cc72bdc4789a1f82b5ae669
Medard <rmedard5@gmail.com>
2018-04-18 15:24:04 +0200

    EC

--- template.php
+++ template.php
@@ -9,7 +9,8 @@ include ('parser/simple_html_dom.php');
 function jirboot_preprocess_block($variables){
     if ($variables['block']->delta == 'menu-jobs-menu'){
 //        $all_menu = menu_load_all();
-        $jobs_menu = menu_tree_all_data('menu-jobs-menu');
+//        $jobs_menu = menu_tree_all_data('menu-jobs-menu');
+        $jobs_menu = menu_load_links('menu-jobs-menu');
         print_r($jobs_menu);
     }
 }
