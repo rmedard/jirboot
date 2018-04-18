be4eabbc075848c7fb0f2a961827392b38cdea31
rmedard <00Derfine@5>
2018-04-18 21:14:16 +0200

    W10

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -155,8 +155,8 @@
                 <?php if (!empty($page['highlighted'])): ?>
                     <div class="highlighted jumbotron"><?php print render($page['highlighted']); ?></div>
                 <?php endif; ?>
-<!--                --><?php //if (!empty($breadcrumb)): print $breadcrumb;
-//                endif; ?>
+                <?php if (!empty($breadcrumb)): print $breadcrumb;
+                endif; ?>
                 <a id="main-content"></a>
                 <?php print render($title_prefix); ?>
                 <?php if (!empty($title)): ?>
@@ -170,9 +170,9 @@
                 <?php if (!empty($page['help'])): ?>
                     <?php print render($page['help']); ?>
                 <?php endif; ?>
-<!--                --><?php //if (!empty($action_links)): ?>
-<!--                    <ul class="action-links">--><?php //print render($action_links); ?><!--</ul>-->
-<!--                --><?php //endif; ?>
+                <?php if (!empty($action_links)): ?>
+                    <ul class="action-links"><?php print render($action_links); ?></ul>
+                <?php endif; ?>
                 <?php print render($page['content']); ?>
             </section>
 
