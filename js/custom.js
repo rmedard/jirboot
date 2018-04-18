53029bd47327dd246f919c96a8779e805e017252
rmedard <00Derfine@5>
2018-04-18 19:52:46 +0200

    W10

--- js/custom.js
+++ js/custom.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
     var isMobile = window.matchMedia("only screen and (max-width: 480px)");
 
     if (isMobile.matches) {
+        $('body').css('background-color', 'red');
         $('section#block-menu-menu-jobs-menu > ul.nav').removeClass('nav-justified');
     }
 });
