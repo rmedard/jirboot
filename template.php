63bcaa77ad05bb33c0d3201c7aeeab7732899e9c
Medard <rmedard5@gmail.com>
2018-04-18 18:01:36 +0200

    EC

--- template.php
+++ template.php
@@ -8,7 +8,7 @@ include ('parser/simple_html_dom.php');
 
 function jirboot_preprocess_page(&$variables){
 //    var_dump($variables['content_column_class']);
-    $variables['content_column_class'] = 'col-sm-8';
+    $variables['content_column_class'] = 'col-sm-8 col-md-8';
 }
 
 function jirboot_preprocess_block(&$variables){
