c270573f69b00cbf08f777091b61865a61c6c349
Medard <rmedard5@gmail.com>
2018-04-18 14:26:51 +0200

    EC

--- template.php
+++ template.php
@@ -1,6 +1,11 @@
 <?php
 
+include ('parser/simple_html_dom.php');
 /**
  * @file
  * The primary PHP file for this theme.
  */
+
+function jirboot_preprocess_block($variables){
+    dpm($variables);
+}
