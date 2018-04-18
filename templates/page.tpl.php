9f466ac2cd2a38ff6fce5bfbb4bfa997aee05447
rmedard <00Derfine@5>
2018-04-18 20:56:04 +0200

    W10

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -164,15 +164,15 @@
                 <?php endif; ?>
                 <?php print render($title_suffix); ?>
                 <?php print $messages; ?>
-<!--                --><?php //if (!empty($tabs)): ?>
-<!--                    --><?php //print render($tabs); ?>
-<!--                --><?php //endif; ?>
+                <?php if (!empty($tabs)): ?>
+                    <?php print render($tabs); ?>
+                <?php endif; ?>
                 <?php if (!empty($page['help'])): ?>
                     <?php print render($page['help']); ?>
                 <?php endif; ?>
-                <?php if (!empty($action_links)): ?>
-                    <ul class="action-links"><?php print render($action_links); ?></ul>
-                <?php endif; ?>
+<!--                --><?php //if (!empty($action_links)): ?>
+<!--                    <ul class="action-links">--><?php //print render($action_links); ?><!--</ul>-->
+<!--                --><?php //endif; ?>
                 <?php print render($page['content']); ?>
             </section>
 
