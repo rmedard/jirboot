cf1b31b32fe6c0d0933a3ecd455049d08e6bfa0c
rmedard <00Derfine@5>
2018-04-19 21:47:37 +0200

    W10

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -121,6 +121,22 @@
                     <span class="icon-bar"></span>
                 </button>
             <?php endif; ?>
+            <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
+                <div class="navbar-collapse collapse"
+                     id="navbar-collapse">
+                    <nav role="navigation">
+                        <?php if (!empty($primary_nav)): ?>
+                            <?php print render($primary_nav); ?>
+                        <?php endif; ?>
+                        <?php if (!empty($secondary_nav)): ?>
+                            <?php print render($secondary_nav); ?>
+                        <?php endif; ?>
+                        <?php if (!empty($page['navigation'])): ?>
+                            <?php print render($page['navigation']); ?>
+                        <?php endif; ?>
+                    </nav>
+                </div>
+            <?php endif; ?>
         </div>
     </div>
 </header>
@@ -141,22 +157,22 @@
 <!--                                <span class="icon-bar"></span>-->
 <!--                            </button>-->
 <!--                        --><?php //endif; ?>
-                        <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
-                            <div class="navbar-collapse collapse"
-                                 id="navbar-collapse">
-                                <nav role="navigation">
-                                    <?php if (!empty($primary_nav)): ?>
-                                        <?php print render($primary_nav); ?>
-                                    <?php endif; ?>
-                                    <?php if (!empty($secondary_nav)): ?>
-                                        <?php print render($secondary_nav); ?>
-                                    <?php endif; ?>
-                                    <?php if (!empty($page['navigation'])): ?>
-                                        <?php print render($page['navigation']); ?>
-                                    <?php endif; ?>
-                                </nav>
-                            </div>
-                        <?php endif; ?>
+<!--                        --><?php //if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
+<!--                            <div class="navbar-collapse collapse"-->
+<!--                                 id="navbar-collapse">-->
+<!--                                <nav role="navigation">-->
+<!--                                    --><?php //if (!empty($primary_nav)): ?>
+<!--                                        --><?php //print render($primary_nav); ?>
+<!--                                    --><?php //endif; ?>
+<!--                                    --><?php //if (!empty($secondary_nav)): ?>
+<!--                                        --><?php //print render($secondary_nav); ?>
+<!--                                    --><?php //endif; ?>
+<!--                                    --><?php //if (!empty($page['navigation'])): ?>
+<!--                                        --><?php //print render($page['navigation']); ?>
+<!--                                    --><?php //endif; ?>
+<!--                                </nav>-->
+<!--                            </div>-->
+<!--                        --><?php //endif; ?>
                     </div>
                 </div>
             </div>
