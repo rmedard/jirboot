fc4b326a0ca41937cd03f5908aedea565be608f1
rmedard <00Derfine@5>
2018-04-20 00:19:33 +0200

    W10

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -142,13 +142,13 @@
             </div>
         <?php endif; ?>
     </div>
-    <div class="col-xs-12" id="mobile-banner-region">
-        <?php print render($page['jirheader_banner']); ?>
-    </div>
 </header>
 
 <div id="super-container">
     <div class="super-header">
+        <div class="col-xs-12" id="mobile-banner-region">
+            <?php print render($page['jirheader_banner']); ?>
+        </div>
         <div class="container-fluid">
             <div class="navbar navbar-static-top navbar-default">
                 <div class="container">
