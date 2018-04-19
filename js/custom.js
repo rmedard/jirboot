c15d680409ab14b3b8023acda728b869465a9aec
Medard <rmedard5@gmail.com>
2018-04-19 13:49:40 +0200

    EC

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,11 @@
 (function ($) {
-    $('body.front').css('color', 'red');
-    if (window.matchMedia("(min-width: 400px)").matches) {
-        $('body.front').css('background-color', 'blue');
-        /* the viewport is at least 400 pixels wide */
-    } else {
-        $('body.front').css('background-color', 'red');
-        $('section#block-menu-menu-jobs-menu > ul.nav').removeClass('nav-justified');
-        /* the viewport is less than 400 pixels wide */
-    }
+    $("body.front").css("color", "red");
+    // if (window.matchMedia("(min-width: 400px)").matches) {
+    //     $('body.front').css('background-color', 'blue');
+    //     /* the viewport is at least 400 pixels wide */
+    // } else {
+    //     $('body.front').css('background-color', 'red');
+    //     $('section#block-menu-menu-jobs-menu > ul.nav').removeClass('nav-justified');
+    //     /* the viewport is less than 400 pixels wide */
+    // }
 })(jQuery);
