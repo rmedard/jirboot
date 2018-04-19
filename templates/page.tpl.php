4db510343b35dfe190f968572264ff17913c208d
rmedard <00Derfine@5>
2018-04-19 21:31:02 +0200

    W10

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -105,7 +105,7 @@
         <div class="navbar-header">
             <?php if ($logo): ?>
                 <a class="logo navbar-btn pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
-                    <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+                    <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" id="logo-img"/>
                 </a>
             <?php endif; ?>
 
@@ -122,22 +122,6 @@
                 </button>
             <?php endif; ?>
         </div>
-
-<!--        --><?php //if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
-<!--            <div class="navbar-collapse collapse" id="navbar-collapse">-->
-<!--                <nav role="navigation">-->
-<!--                    --><?php //if (!empty($primary_nav)): ?>
-<!--                        --><?php //print render($primary_nav); ?>
-<!--                    --><?php //endif; ?>
-<!--                    --><?php //if (!empty($secondary_nav)): ?>
-<!--                        --><?php //print render($secondary_nav); ?>
-<!--                    --><?php //endif; ?>
-<!--                    --><?php //if (!empty($page['navigation'])): ?>
-<!--                        --><?php //print render($page['navigation']); ?>
-<!--                    --><?php //endif; ?>
-<!--                </nav>-->
-<!--            </div>-->
-<!--        --><?php //endif; ?>
     </div>
 </header>
 
