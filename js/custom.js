3f88dd2d8a9e1595510b1ed87ee97d19ca1afa6c
Medard <rmedard5@gmail.com>
2018-04-19 14:17:27 +0200

    EC

--- js/custom.js
+++ js/custom.js
@@ -6,4 +6,7 @@ jQuery(document).ready(function($) {
         $('section#block-menu-menu-jobs-menu > ul.nav').removeClass('nav-justified');
         /* the viewport is less than 400 pixels wide */
     }
+
+    var currentUrl = window.location.href;
+    console.log("Current Url: " + currentUrl);
 });
