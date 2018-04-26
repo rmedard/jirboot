b1eb3734a3eeccefa9dc6861efc9627a1f501705
rmedard <00Derfine@5>
2018-04-26 02:46:26 +0200

    W10

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,3 @@
-function handleOrientationChange(mql, $) {
-    if (mql.matches){
-        $('div#jir-footer > div > div').addClass('col-xs-6');
-    }
-}
-
 function handleDeviceChange(deviceMql, $) {
     if (deviceMql.matches){
         $('button#menu-toggle-btn').hide();
@@ -12,11 +6,6 @@ function handleDeviceChange(deviceMql, $) {
         if (window.matchMedia("(orientation: landscape)").matches){
             $('div#jir-footer > div > div').addClass('col-xs-6');
         }
-
-
-        // let orientationMql = window.matchMedia("(orientation: landscape)");
-        // orientationMql.addListener(handleOrientationChange);
-        // handleOrientationChange(orientationMql, $);
     }
 }
 
@@ -24,19 +13,6 @@ jQuery(document).ready(function($) {
     let deviceMql = window.matchMedia("(min-width: 768px)");
     deviceMql.addListener(handleDeviceChange);
     handleDeviceChange(deviceMql, $);
-
-    // let orientationMql = window.matchMedia("(orientation: landscape)");
-    // orientationMql.addListener(handleOrientationChange);
-    // handleOrientationChange(orientationMql, $);
-
-    // if (window.matchMedia("(min-width: 768px)").matches) {
-    //     $('button#menu-toggle-btn').hide();
-    // } else {
-    //     $('section#block-menu-menu-jobs-menu > ul.nav').removeClass('nav-tabs nav-justified').addClass('nav-pills');
-    //     // var mediaQueryListener = window.matchMedia("(orientation: landscape)");
-    //     // mediaQueryListener.addListener(handleOrientationChange);
-    //     // handleOrientationChange(mediaQueryListener, $);
-    // }
 
     const pathName = window.location.pathname.toString();
     $('section#block-menu-menu-jobs-menu > ul.nav > li').each(function (index, value) {
