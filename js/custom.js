d3c79ec1375fd8896d7a04fecf4504d9c0f2382d
Medard <rmedard5@gmail.com>
2018-04-26 17:03:26 +0200

    EC

--- js/custom.js
+++ js/custom.js
@@ -28,9 +28,9 @@ jQuery(document).ready(function($) {
     let jirFooterBlockMaxHeight = 0;
 
     $('div#jir-footer > div.row > div.col-sm-3').each(function () {
-        console.log('height: ' + $(this).height);
-        if ($(this).height > jirFooterBlockMaxHeight) {
-            jirFooterBlockMaxHeight = $(this).height;
+        console.log('height: ' + $(this).height());
+        if ($(this).height() > jirFooterBlockMaxHeight) {
+            jirFooterBlockMaxHeight = $(this).height();
         }
     });
 
