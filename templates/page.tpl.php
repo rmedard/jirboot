155326f070fd0b84b9a280b42536be78d4c13e8b
rmedard <00Derfine@5>
2018-05-13 23:18:03 +0200

    W10

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -196,13 +196,13 @@
                     <?php print render($page['content']); ?>
                 </section>
 
-                <?php if (!empty($page['sidebar_first'])): ?>
+                <?php if (!empty($page['sidebar_first']) && !$display_sidebars): ?>
                     <aside class="col-sm-2" role="complementary">
                         <?php print render($page['sidebar_first']); ?>
                     </aside>  <!-- /#sidebar-first -->
                 <?php endif; ?>
 
-                <?php if (!empty($page['sidebar_second'])): ?>
+                <?php if (!empty($page['sidebar_second']) && !$display_sidebars): ?>
                     <aside class="col-sm-2" role="complementary">
                         <?php print render($page['sidebar_second']); ?>
                     </aside>  <!-- /#sidebar-second -->
