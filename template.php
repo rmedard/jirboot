a1aa933865b73d163e447c503aa74951515c46f2
rmedard <00Derfine@5>
2018-05-14 00:20:46 +0200

    W10

--- template.php
+++ template.php
@@ -6,6 +6,7 @@
  */
 
 function jirboot_preprocess_page(&$variables) {
+    $variables['display_sidebars'] = 'true';
     $full_pages = array('services');
     if (in_array(current_path(), $full_pages)) {
         $variables['content_column_class'] = ' class="col-sm-12"';
