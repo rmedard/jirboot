00df2703e2021e7ded59f706404e6b5185598c70
Medard Rebero <medard.rebero@gmail.com>
2019-06-15 02:42:38 +0200

    jiug

--- template.php
+++ template.php
@@ -158,7 +158,7 @@ function jirboot_preprocess_block(&$variables)
         $output .= '</ul>';
         $variables['content'] = $output;
     } elseif ($variables['block']->delta == 'jir_realtime') {
-        $output = '<div class="panel panel-success">';
+        $output = '<div class="panel panel-info">';
         $output .= '<div class="panel-heading"><i class="fa fa-bolt"></i> ';
         $output .= $variables['block']->subject;
         $output .= '</div>';
@@ -167,7 +167,7 @@ function jirboot_preprocess_block(&$variables)
         $variables['content'] = $output;
     } elseif (in_array($variables['block']->delta, array('5', '13', '14'))) {
         $icons = array('5' => 'fa-home', '13' => 'fa-briefcase', '14' => 'fa-globe');
-        $output = '<div class="panel panel-success">';
+        $output = '<div class="panel panel-info">';
         $output .= '<div class="panel-heading"><i class="fa ' . $icons[$variables['block']->delta] . '"></i> ';
         $output .= $variables['block']->subject;
         $output .= '</div>';
