376bc2b0e0ba3b32be6f6dcff9f13a238451dfa6
Medard Rebero <medard.rebero@gmail.com>
2019-06-15 14:43:25 +0200

    jiug

--- template.php
+++ template.php
@@ -7,8 +7,6 @@
 
 function jirboot_preprocess_page(&$variables)
 {
-    dpm($variables);
-
     $full_pages = array('services', 'post-advert');
     $management_pages = array('job-applications', 'jobs', 'employers', 'banners', 'faq-manage', 'testimonials-manage', 'news-management');
 
