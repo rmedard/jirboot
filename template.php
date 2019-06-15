9ce3448f8c15fe08633a8bb3dc4adee2c9970aa8
Medard Rebero <medard.rebero@gmail.com>
2019-06-15 18:32:55 +0200

    jiug

--- template.php
+++ template.php
@@ -73,82 +73,82 @@ function jirboot_preprocess_block(&$variables)
             $menu['jobs_count'] = 0;
             switch ($menu['link_path']) {
                 case 'jobs/featured':
-                    $query1 = new EntityFieldQuery();
-                    $featured = $query1->entityCondition('entity_type', 'node')
-                        ->entityCondition('bundle', 'job')
-                        ->propertyCondition('status', NODE_PUBLISHED)
-                        ->fieldCondition('field_posting_type', 'tid', [
-                            33,
-                            34,
-                            36,
-                        ], 'IN')
-                        ->count()->execute();
-                    $menu['jobs_count'] = $featured;
+//                    $query1 = new EntityFieldQuery();
+//                    $featured = $query1->entityCondition('entity_type', 'node')
+//                        ->entityCondition('bundle', 'job')
+//                        ->propertyCondition('status', NODE_PUBLISHED)
+//                        ->fieldCondition('field_posting_type', 'tid', [
+//                            33,
+//                            34,
+//                            36,
+//                        ], 'IN')
+//                        ->count()->execute();
+                    $menu['jobs_count'] = 1;
                     break;
                 case 'jobs/all':
-                    $query2 = new EntityFieldQuery();
-                    $alljobs = $query2->entityCondition('entity_type', 'node')
-                        ->entityCondition('bundle', 'job')
-                        ->propertyCondition('status', NODE_PUBLISHED)
-                        ->fieldCondition('field_offer_type', 'value', 'Job')
-                        ->count()->execute();
-                    $menu['jobs_count'] = $alljobs;
+//                    $query2 = new EntityFieldQuery();
+//                    $alljobs = $query2->entityCondition('entity_type', 'node')
+//                        ->entityCondition('bundle', 'job')
+//                        ->propertyCondition('status', NODE_PUBLISHED)
+//                        ->fieldCondition('field_offer_type', 'value', 'Job')
+//                        ->count()->execute();
+                    $menu['jobs_count'] = 0;
                     break;
                 case 'jobs/tender':
-                    $query3 = new EntityFieldQuery();
-                    $tenders = $query3->entityCondition('entity_type', 'node')
-                        ->entityCondition('bundle', 'job')
-                        ->propertyCondition('status', NODE_PUBLISHED)
-                        ->fieldCondition('field_offer_type', 'value', 'Tender')
-                        ->count()->execute();
-                    $menu['jobs_count'] = $tenders;
+//                    $query3 = new EntityFieldQuery();
+//                    $tenders = $query3->entityCondition('entity_type', 'node')
+//                        ->entityCondition('bundle', 'job')
+//                        ->propertyCondition('status', NODE_PUBLISHED)
+//                        ->fieldCondition('field_offer_type', 'value', 'Tender')
+//                        ->count()->execute();
+                    $menu['jobs_count'] = 1;
                     break;
                 case 'jobs/consultancy':
-                    $query8 = new EntityFieldQuery();
-                    $consultancy = $query8->entityCondition('entity_type', 'node')
-                        ->entityCondition('bundle', 'job')
-                        ->propertyCondition('status', NODE_PUBLISHED)
-//                        ->fieldCondition('field_offer_type', 'value', 'Consultancy')
-                        ->addTag('consultancy_or_freelance')
-                        ->count()->execute();
-                    $menu['jobs_count'] = $consultancy;
+//                    $query8 = new EntityFieldQuery();
+//                    $consultancy = $query8->entityCondition('entity_type', 'node')
+//                        ->entityCondition('bundle', 'job')
+//                        ->propertyCondition('status', NODE_PUBLISHED)
+////                        ->fieldCondition('field_offer_type', 'value', 'Consultancy')
+//                        ->addTag('consultancy_or_freelance')
+//                        ->count()->execute();
+                    $menu['jobs_count'] = 0;
                     break;
                 case 'jobs/internships':
-                    $query4 = new EntityFieldQuery();
-                    $intern = $query4->entityCondition('entity_type', 'node')
-                        ->entityCondition('bundle', 'job')
-                        ->propertyCondition('status', NODE_PUBLISHED)
-                        ->fieldCondition('field_offer_type', 'value', 'Internship')
-                        ->count()->execute();
-                    $menu['jobs_count'] = $intern;
+//                    $query4 = new EntityFieldQuery();
+//                    $intern = $query4->entityCondition('entity_type', 'node')
+//                        ->entityCondition('bundle', 'job')
+//                        ->propertyCondition('status', NODE_PUBLISHED)
+//                        ->fieldCondition('field_offer_type', 'value', 'Internship')
+//                        ->count()->execute();
+                    $menu['jobs_count'] = 0;
                     break;
                 case 'jobs/public-adverts':
-                    $query6 = new EntityFieldQuery();
-                    $emps = $query6->entityCondition('entity_type', 'node')
-                        ->entityCondition('bundle', 'employer')
-                        ->propertyCondition('status', NODE_PUBLISHED)
-                        ->fieldCondition('field_employer_public_employer', 'value', 1)
-                        ->execute();
-                    $publics = 0;
-                    if (isset($emps['node'])) {
-                        $emps_ids = array_keys($emps['node']);
-                        $query7 = new EntityFieldQuery();
-                        $publics = $query7->entityCondition('entity_type', 'node')
-                            ->entityCondition('bundle', 'job')
-                            ->propertyCondition('status', NODE_PUBLISHED)
-                            ->fieldCondition('field_employer', 'target_id', $emps_ids, 'IN')
-                            ->count()->execute();
-                    }
-                    $menu['jobs_count'] = $publics;
+//                    $query6 = new EntityFieldQuery();
+//                    $emps = $query6->entityCondition('entity_type', 'node')
+//                        ->entityCondition('bundle', 'employer')
+//                        ->propertyCondition('status', NODE_PUBLISHED)
+//                        ->fieldCondition('field_employer_public_employer', 'value', 1)
+//                        ->execute();
+//                    $publics = 0;
+//                    if (isset($emps['node'])) {
+//                        $emps_ids = array_keys($emps['node']);
+//                        $query7 = new EntityFieldQuery();
+//                        $publics = $query7->entityCondition('entity_type', 'node')
+//                            ->entityCondition('bundle', 'job')
+//                            ->propertyCondition('status', NODE_PUBLISHED)
+//                            ->fieldCondition('field_employer', 'target_id', $emps_ids, 'IN')
+//                            ->count()->execute();
+//                    }
+                    $menu['jobs_count'] = 1;
                     break;
                 case 'jobs/others':
-                    $query5 = new EntityFieldQuery();
-                    $others = $query5->entityCondition('entity_type', 'node')
-                        ->entityCondition('bundle', 'job')
-                        ->propertyCondition('status', NODE_PUBLISHED)
-                        ->fieldCondition('field_offer_type', 'value', 'Other')
-                        ->count()->execute();
-                    $menu['jobs_count'] = $others;
+//                    $query5 = new EntityFieldQuery();
+//                    $others = $query5->entityCondition('entity_type', 'node')
+//                        ->entityCondition('bundle', 'job')
+//                        ->propertyCondition('status', NODE_PUBLISHED)
+//                        ->fieldCondition('field_offer_type', 'value', 'Other')
+//                        ->count()->execute();
+                    $menu['jobs_count'] = 0;
                     break;
             }
 
