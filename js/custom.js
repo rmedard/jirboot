76f02a4b6ad340baa8fafd8ca769b742b89376de
Medard Rebero <medard.rebero@gmail.com>
2019-10-22 02:26:02 +0200

    jiug

--- js/custom.js
+++ js/custom.js
@@ -12,7 +12,7 @@ jQuery(document).ready(function($) {
     handleDeviceChange(deviceMql, $);
 
     // Disable ajax upload for file field
-    drupal_add_js("jQuery(function() { jQuery('#edit-field-image :submit').unbind(); });", 'inline');
+    drupal_add_js("jQuery(function() { jQuery('#edit-field-cv-resume-attachment-und-0-upload :submit').unbind(); });", 'inline');
 
     const pathName = window.location.pathname.toString();
     $('section#block-menu-menu-jobs-menu > ul.nav > li').each(function (index, value) {
