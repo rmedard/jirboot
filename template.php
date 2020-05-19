10b93734ddec7133bac398cd95853e58e5e422fd
Medard Rebero <medard.rebero@gmail.com>
2020-05-19 19:27:58 +0200

    W10

--- template.php
+++ template.php
@@ -13,7 +13,7 @@ function jirboot_preprocess_html(&$variables)
     $variables['check_js_enabled'] = false;
 //    if (substr_compare(current_path(), 'job/', 0, strlen('job/')) === 0) {
     var_dump(current_path());
-    if (substr(current_path(), 0, strlen('job/')) === 'job/') {
+    if (substr(current_path(), 0, strlen('node/')) === 'node/') {
         $variables['check_js_enabled'] = true;
     }
 }
